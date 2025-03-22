d96f0a0fa57fdac32a4fe4c9f8af2fa42ef5366d
SharpIce <VupRbl@163.com>
2025-03-22 17:51:13 +0800

    存档

--- project/website/hexo-local-url/src/main.js
+++ project/website/hexo-local-url/src/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var hexo = hexo || {};
-
 if (hexo.env.cmd === 'server') {
-	hexo.config.url = `http://${hexo.config.server.ip || 'localhost'}:${hexo.config.server.port}`;
+	//hexo.config.url = `http://${hexo.config.server.ip || 'localhost'}:${hexo.config.server.port}`;
+	hexo.config.url = `http://${hexo.config.server.ip || 'localhost'}:8200`;
 }
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,9 @@
+import type Hexo from 'hexo';
+
+declare global {
+	const hexo: Hexo;
+}
+
 declare module '@fancyapps/ui/dist/fancybox/l10n/zh_CN.esm.js' {
 	export const zh_CN: Record<string, string>;
 }
